5250d24110141a91136b49cbf9c513c6195025a9
Novarima <vaidik.manori2021@vitstudent.ac.in>
2023-06-24 15:53:32 +0530

    JS added and changes made

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webd_clk
 Basic clock using HTML, CSS and JS
+
+In JS, the methods change the value of the text in ids. Loop happens which updates the value.
---
+++ app.js
@@ -0,0 +1,21 @@
+function loop(){
+    const f_time = new Date();
+    const hh = f_time.getHours();
+    const mm = f_time.getMinutes();
+    const ss = f_time.getSeconds();
+
+    if(hh < 10)
+        hh = "0" + hh;
+
+    if(hh < 10)
+        mm = "0" + mm;
+
+    if(hh < 10)
+        ss = "0" + ss;
+
+    document.getElementById('hour').innerHTML = hh + ":";
+    document.getElementById('minute').innerHTML = mm + ":";
+    document.getElementById('second').innerHTML = ss;
+}
+
+setInterval(loop, 100);
